fcf8c6e02db1bf197992ad9d4d414d1180a085ed
Samiran Samanta <samirans170@gmail.com>
2024-07-18 23:55:08 +0530

    Find Second Largest Element

---
+++ SecondLargestElement.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SecondLargestElement {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        for(int i = 0; i<size; i++){
+            System.out.print("Enter the element: ");
+            arr[i] = input.nextInt();
+        }
+    }
+
+    public static int findSecondLargest(int[] arr){
+        if(arr.length == 0){
+            return -1;
+        }
+    }
+}
