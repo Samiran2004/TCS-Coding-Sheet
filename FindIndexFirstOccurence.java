cad979655883c85c341a79c38b617ebea928e050
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2024-09-20 19:12:30 +0530

    Find Index of First Occurence

--- FindIndexFirstOccurence.java
+++ FindIndexFirstOccurence.java
@@ -1,14 +1,43 @@
 import java.util.Scanner;
 
 public class FindIndexFirstOccurence {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the size of the string array: ");
-        int size = input.nextInt();
-        String[] strArr = new String[size];
-        for(int i = 0; i<size; i++){
-            System.out.print("Enter the string: ");
-            strArr[i] = input.next();
+        System.out.print("Enter the heystack string: ");
+        String heyString = input.next();
+        System.out.print("Enter the needle: ");
+        String needleString = input.next();
+        input.close();
+        System.out.println(findTheIndexOfFirst(heyString, needleString));
+    }
+
+    private static int findTheIndexOfFirst(String hayStack, String needle) {
+        int hayStackLength = hayStack.length();
+        int needleLength = needle.length();
+        if (hayStackLength < needleLength) {
+            return -1;
         }
+        for (int i = 0; i < hayStackLength; i++) {
+            if (hayStack.charAt(i) == needle.charAt(0)) {
+                if (compare(hayStack, needle, i) == true) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static boolean compare(String heyString, String needleString, int idx) {
+        int n1 = heyString.length();
+        int n2 = needleString.length();
+        for (int i = 0; i < n2; i++) {
+            if (idx >= n1) {
+                return false;
+            }
+            if (heyString.charAt(idx++) != needleString.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
