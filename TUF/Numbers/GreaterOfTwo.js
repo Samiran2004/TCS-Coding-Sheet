0b889805b4e51211bb52ffe014baed09c3d47084
Samiran2004 <samirans170@gmail.com>
2025-02-01 00:35:36 +0530

    add/Greatest of two numbers[TypeScript]

---
+++ TUF/Numbers/GreaterOfTwo.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function findGreatest(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+var num1 = 4;
+var num2 = 10;
+console.log(findGreatest(num1, num2));
